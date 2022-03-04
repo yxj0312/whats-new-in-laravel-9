6268eaa7459fac082fe7339cfa6b1f46c3cbe0f0
yxj0312 <yanxiajun@hotmail.com>
2022-03-04 21:35:59 +0100

    L01 - Controller Router Groups

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::controller(PostsController::class)->group(function() {
+    Route::get('/posts', 'index');
+    Route::get('/posts/{post}', 'show');
+    Route::post('/posts/', 'store');
 });
