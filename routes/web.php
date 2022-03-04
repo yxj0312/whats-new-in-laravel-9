6300ecd1b0bc7db903d89bb54e1bca08e78a382f
yxj0312 <yanxiajun@hotmail.com>
2022-03-04 22:06:15 +0100

    L03 - New Helper Functions

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return str('hello world')->upper();
+    // return Str::of('hello world')->upper();
+    // return view('welcome');
+})->name('home');
+
+Route::get('/endpoint', function () {
+    // return redirect()->route('name');
+    return to_route('home');
 });
 
 
