d2ab79201cb09c2bbe3d411b70fcb04438b403c1
yxj0312 <yanxiajun@hotmail.com>
2022-03-08 21:04:15 +0100

    L09 - Full Text Indexing

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -10,6 +10,7 @@ class Post extends Model
 {
     use HasFactory, Searchable;
 
+    #[SearchUsingFullText('body')]
     public function toSearchableArray()
     {
         return [
--- database/migrations/2022_03_05_214323_create_posts_table.php
+++ database/migrations/2022_03_05_214323_create_posts_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->id();
             $table->foreignIdFor(User::class)->constrained()->onDelete('cascade');
             $table->string('title');
-            $table->text('body');
+            $table->text('body')->fulltext();
             $table->timestamps();
         });
     }
