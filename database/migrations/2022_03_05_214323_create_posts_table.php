06fb7074b9e1ede32fa3eef2e311bf84cf2f0da6
yxj0312 <yanxiajun@hotmail.com>
2022-03-08 23:08:04 +0100

    L10 - Enum Attribute Casting - p1

--- database/migrations/2022_03_05_214323_create_posts_table.php
+++ database/migrations/2022_03_05_214323_create_posts_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Enums\PostState;
 use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
@@ -19,6 +20,8 @@ return new class extends Migration
             $table->foreignIdFor(User::class)->constrained()->onDelete('cascade');
             $table->string('title');
             $table->text('body')->fulltext();
+            $table->enum('state', PostState::class);
+            // $table->string('state')->default('draft');
             $table->timestamps();
         });
     }
