4fdc84d9c6d36623e3936e4a0cbf2d8829a573c5
yxj0312 <yanxiajun@hotmail.com>
2022-04-02 22:43:58 +0200

    install breeze

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,5 @@
 <?php
 
-use App\Enums\PostState;
-use App\Http\Controllers\PostsController;
-use App\Models\Post;
-use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,62 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    $post = new Post;
-
-    $post->user_id = 1;
-    $post->title= 'My title';
-    $post->body = 'My body';
-
-    $post->state = PostState::Published;
-
-    $post->save();
-
-    return 'Done';
-    // return Post::first()->state->value;
-    
-    // return Post::search('voluptatem')->paginate();
-
-
-    // return Blade::render('{{ $greeting }}, World', ['greeting' => 'Hello']);
-
-    // throw new Exception("Error Processing Request", 1);
-    
-    // return str('hello world')->upper();
-    // return Str::of('hello world')->upper();
-    // return view('welcome');
-})->name('home');
-
-Route::get('/endpoint', function () {
-    // return redirect()->route('name');
-    return to_route('home');
+    return view('welcome');
 });
 
-// Route::get('/posts/{state}', function(PostState $state){
-//     dd($state);
-// });
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
-
-// Route::controller(PostsController::class)->group(function() {
-//     Route::get('/posts', 'index');
-//     Route::get('/posts/{post}', 'show');
-//     Route::post('/posts/', 'store');
-// });
-
-
-// Route::get('/users/{user}/posts/{post:id}', function(User $user, Post $post){
-//     return $post;
-// });
-
-Route::get('/users/{user}/posts/{post}', function(User $user, Post $post){
-    return $post;
-})->scopeBindings();
-
-Route::get('/about', function() {
-    $name = 'foo';
-
-    return 'The About Page';
-});
-
-Route::get('/posts/{post}', function(Post $post){
-    return $post;
-})->name('posts.show');
+require __DIR__.'/auth.php';
