cc031622225a0b53aaae0abd307e72ac4954634d
yxj0312 <yanxiajun@hotmail.com>
2022-04-25 23:12:00 +0200

    add prep

---
+++ resources/views/images/index.blade.php
@@ -0,0 +1,5 @@
+
+<div class="max-w-lg mx-auto mt-24">
+    <h1 class="text-4xl font-bold text-center">Image Uploader</h1>
+
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ImageController;
 use App\Http\Controllers\UsersController;
 use Illuminate\Support\Facades\Route;
 
@@ -19,6 +20,8 @@ Route::get('/', function () {
 });
 
 Route::get('/users', [UsersController::class, 'index']);
+
+Route::get('/images', [ImageController::class, 'index']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
