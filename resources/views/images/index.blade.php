a9022ed182ee4572afd0e23d5e683e973e0fe296
yxj0312 <yanxiajun@hotmail.com>
2022-04-26 22:26:54 +0200

    update index blade

--- resources/views/images/index.blade.php
+++ resources/views/images/index.blade.php
@@ -1,5 +1,11 @@
+<x-app-layout>
+    <x-slot name="header">
+        Image Uploader
+    </x-slot>
+    <div class="py-12 bg-white">
+        <div class="max-w-lg mx-auto mt-24">
+            <h1 class="text-4xl font-bold text-center">Image Uploader</h1>
 
-<div class="max-w-lg mx-auto mt-24">
-    <h1 class="text-4xl font-bold text-center">Image Uploader</h1>
-
-</div>
+        </div>
+    </div>
+</x-app-layout>
