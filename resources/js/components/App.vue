20898522a2b39c8c29a8e0ef46db1811bcf2258a
yxj0312 <yanxiajun@hotmail.com>
2022-04-28 22:32:12 +0200

    install filepond and use it

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,11 +1,26 @@
 <template>
     <div>
         <h2 class="text-xl text-center">{{ message }}</h2>
+        <file-pond
+            name="image"
+            ref="pond"
+            label-idle="Click to choose image, or drag here..."
+            server="/upload"
+        ></file-pond>
     </div>
 </template>
 
 <script>
+import vueFilePond from 'vue-filepond';
+import "filepond/dist/filepond.min.css";
+
+const FilePond = vueFilePond();
+
 export default {
+    components: {
+        FilePond
+    },
+    
     data() {
         return {
             message: 'Hello, world'
