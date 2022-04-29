680320017733c7d6769351249844851d2573e1f7
yxj0312 <yanxiajun@hotmail.com>
2022-04-29 23:07:02 +0200

    add store method

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Image;
 use Illuminate\Http\Request;
 
 class ImageController extends Controller
@@ -16,8 +17,30 @@ class ImageController extends Controller
         
     }
 
-    public function store()
+    public function store(Request $request)
     {
-        
+        if (!$request->hasFile('image')) {
+            return response()->json(['error' => 'There is no image present.'], 400);
+        }
+
+        $request->validate([
+            'image' => 'required|file|image'
+        ]);
+
+        $path = $request->file('image')->store('public/images');
+
+        if ($path) {
+            return response()->json(['error', 'The file could not be saved'], 500);
+        }
+
+        $uploadedFile = $request->file('image');
+
+        $image = Image::create([
+            'name' => $uploadedFile->hashName(),
+            'extension' => $uploadedFile->extension(),
+            'size' => $uploadedFile->getSize()
+        ]);
+
+        return $image;
     }
 }
