8fe02cacd6a3ced91b0d0927437fb91b5570d8ea
yxj0312 <yanxiajun@hotmail.com>
2022-06-06 23:08:15 +0200

    add ep6-2 router

--- routes/web.php
+++ routes/web.php
@@ -67,3 +67,7 @@ Route::get('/css-grids/ep05-2', function () {
 Route::get('/css-grids/ep06-1', function () {
     return view('css-grids/restaurant-ratings');
 });
+
+Route::get('/css-grids/ep06-2', function () {
+    return view('css-grids/profile-card');
+});
