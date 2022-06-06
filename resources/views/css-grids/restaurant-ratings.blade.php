a52a64531aacb6118f17b76328f63ff033f2ea7d
yxj0312 <yanxiajun@hotmail.com>
2022-06-06 21:57:50 +0200

    change base css loacation

--- resources/views/css-grids/restaurant-ratings.blade.php
+++ resources/views/css-grids/restaurant-ratings.blade.php
@@ -3,7 +3,7 @@
    <head>
       <title>Restaurant Listing</title>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <link rel="stylesheet" href="../../base.css" />
+      <link rel="stylesheet" href="../../css/base.css" />
       <link
          rel="stylesheet"
          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
