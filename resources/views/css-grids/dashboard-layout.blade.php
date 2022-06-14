7c011df3eaf439c2470e1e393d9bfbbb7b744fb7
yxj0312 <yanxiajun@hotmail.com>
2022-06-14 21:49:36 +0200

    add ep12 layout

--- resources/views/css-grids/dashboard-layout.blade.php
+++ resources/views/css-grids/dashboard-layout.blade.php
@@ -12,4 +12,48 @@
          referrerpolicy="no-referrer"
       />
    </head>
+    <body>
+      <section class="left">
+         <nav>
+            <div>
+               <img src="{{asset('/images/logo.svg')}}" alt="Logo" height="40" />
+            </div>
+         </nav>
+      </section>
+      <section class="main">
+         <h3>Dashboard</h3>
+         <div class="container">
+            <div class="block block1">
+               <i class="fa-solid fa-money-bill-wave"></i>
+               <i class="fa-solid fa-ellipsis"></i>
+               <div>
+                  <h3>$12,766</h3>
+                  <p>Today's Revenue</p>
+               </div>
+               <span> + 13% </span>
+            </div>
+            <div class="block block1">
+               <p>Block 1</p>
+            </div>
+            <div class="block block1">
+               <p>Block 1</p>
+            </div>
+            <div class="block block1">
+               <p>Block 1</p>
+            </div>
+            <div class="block block2">
+               <p>Block 2</p>
+            </div>
+            <div class="block block3">
+               <p>Block 3</p>
+            </div>
+            <div class="block block4">
+               <p>Block 4</p>
+            </div>
+            <div class="block block5">
+               <p>Block 5</p>
+            </div>
+         </div>
+      </section>
+   </body>
 </html>
