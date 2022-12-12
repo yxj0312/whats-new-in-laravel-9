5acf6ce76af6644aec061caa313f185c01d91dcf
yxj0312 <yanxiajun@hotmail.com>
2022-12-12 21:12:48 +0100

    change factory seed to 1000

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -16,7 +16,7 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        for ($i = 0; $i < 10; $i++) {
+        for ($i = 0; $i < 1000; $i++) {
             $company = Company::factory()->create();
 
 
