89eb8352571dee87d1d0904744a112deaf2b65b6
yxj0312 <yanxiajun@hotmail.com>
2022-12-17 22:34:04 +0100

    select what u need to reduce the memory usage

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -26,10 +26,11 @@ class PostController extends Controller
     public function index()
     {
         $years = Post::query()
+            ->select('id', 'title', 'slug', 'published_at', 'author_id')
             ->with('author')
-            ->latest('updated_at')
+            ->latest('published_at')
             ->get()
-            ->groupBy(fn ($post) => $post->updated_at->year);
+            ->groupBy(fn ($post) => $post->published_at->year);
 
         return view('posts.index', [
             'years' => $years
