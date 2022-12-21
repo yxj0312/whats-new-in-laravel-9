c3295fd3f01502757fbba37dcd087360c1401b90
yxj0312 <yanxiajun@hotmail.com>
2022-12-21 22:26:18 +0100

    change blade display codes

--- resources/views/users/index2.blade.php
+++ resources/views/users/index2.blade.php
@@ -51,7 +51,8 @@
                 </td>
                 <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 text-gray-500">
                   <!-- {{ $user->last_login_at->diffForHumans() }} -->
-                  {{ $user->logins()->latest()->first()->created_at->diffForHumans() }}
+                  <!-- {{ $user->logins()->latest()->first()->created_at->diffForHumans() }} -->
+                  {{ $user->logins->sortByDesc('created_at')->first()->created_at->diffForHumans() }}
                 </td>
                 <td class="px-6 py-4 whitespace-no-wrap text-right border-b border-gray-200 text-sm leading-5 font-medium">
                   <a href="#" class="text-indigo-600 hover:text-indigo-900 focus:outline-none focus:underline">Edit</a>
