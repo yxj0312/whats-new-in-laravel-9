9a8b814b2a2e03990d93bf7aee304c2249332345
yxj0312 <yanxiajun@hotmail.com>
2023-01-03 21:50:21 +0100

    add feature comments relationship

--- app/Models/Feature.php
+++ app/Models/Feature.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Feature extends Model
 {
     use HasFactory;
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
 }
