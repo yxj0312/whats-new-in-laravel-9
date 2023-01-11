d7dc9f15c4962d6bba7a3752635a036109440065
yxj0312 <yanxiajun@hotmail.com>
2023-01-11 22:29:32 +0100

    add photo column to users table

--- database/migrations/2022_12_10_000000_create_users_table.php
+++ database/migrations/2022_12_10_000000_create_users_table.php
@@ -26,6 +26,7 @@ return new class extends Migration
             // $table->timestamps();
             $table->id();
             $table->string('name');
+            $table->string('photo')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
